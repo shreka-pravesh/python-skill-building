4af2ac77cfd21c99112af9cd2e09da3264f89c2d
shreka-pravesh <shrekadhanu@gmail.com>
2025-10-06 21:13:08 +0530

    Create know_your_type.py
    Added to display each variable's data type using type()  program.
    Concept: Variables and data types.

---
+++ Day01_Basics/know_your_type.py
@@ -0,0 +1,11 @@
+# This program shows what type of value each variable holds
+
+age = 20
+height = 5.6
+name = "Shreka"
+is_happy = True
+
+print("Age:", age, "is of type", type(age))
+print("Height:", height, "is of type", type(height))
+print("Name:", name, "is of type", type(name))
+print("Is Happy:", is_happy, "is of type", type(is_happy))
