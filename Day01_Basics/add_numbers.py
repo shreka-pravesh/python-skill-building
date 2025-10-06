3ed8a2ee8d81790e287b0bfa4e44a2093c9db67d
shreka-pravesh <shrekadhanu@gmail.com>
2025-10-06 21:08:13 +0530

    Create add_numbers.py
    Added simple addition of two user-entered numbers and shows the total program.
    Concept: User input, type conversion and arithmetic.

---
+++ Day01_Basics/add_numbers.py
@@ -0,0 +1,8 @@
+# This program adds two numbers entered by the user
+
+print("Let's add two numbers together!")
+first = float(input("Enter the first number: "))
+second = float(input("Enter the second number: "))
+
+total = first + second
+print("The total is:", total)
